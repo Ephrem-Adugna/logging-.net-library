958c4036a1b7cd5fed1d636d3b5f324bd1a475eb
Ephrem-Adugna <ephremadugna1219@gmail.com>
2021-05-31 22:26:26 -0700

    Initial Commit

---
+++ Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+namespace logginglib
+{
+    public class Logger
+    {
+
+        private readonly ILogger<Logger> _logger;
+public Logger(ILogger<Logger> logger = null)
+{
+            _logger = logger;
+        }
+
+        public void DebugLog(string message){
+            _logger?.LogDebug(message);
+        }
+        public void InfoLog(string message)
+        {
+            _logger?.LogInformation(message);
+        }
+        public void WarnLog(string message)
+        {
+            _logger?.LogWarning(message);
+        }
+        public void ErrorLog(string message)
+        {
+            _logger?.LogError(message);
+        }
+        public void CriticalLog(string message)
+        {
+            _logger?.LogCritical(message);
+        }
+        public void TraceLog(string message)
+        {
+            _logger?.LogTrace(message);
+        }
+    }
+}
